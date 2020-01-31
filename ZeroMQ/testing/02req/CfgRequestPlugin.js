bffab501b3fe91fb5cd6309439fdb241e99b611d
Ricky <etoricky@gmail.com>
2020-01-31 23:32:03 +0800

    CfgRequestPlugin

--- ZeroMQ/testing/02req/CfgRequestPlugin.js
+++ ZeroMQ/testing/02req/CfgRequestPlugin.js
@@ -3,7 +3,7 @@ var zmq = require('zeromq')
 var socket = zmq.socket('req')
 socket.connect('tcp://localhost:19412')
 socket.on('message', function(msg) {
-  console.log(JSON.parse(msg.toString()));
+  console.log(JSON.stringify(JSON.parse(msg.toString()), null, 2));
 })
-var dic = {'function':'CfgRequestFeeder'}
+var dic = {'function':'CfgRequestPlugin'}
 socket.send(JSON.stringify(dic));

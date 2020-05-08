25440890594a5c55a8e63962eb55acbe6b803ee8
Ricky <etoricky@gmail.com>
2020-05-08 15:49:47 +0800

    cleaner code

--- ProtocolBuffer/py-grpc/client.py
+++ ProtocolBuffer/py-grpc/client.py
@@ -5,9 +5,6 @@ import helloworld_pb2
 import helloworld_pb2_grpc
 
 def run():
-    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-    # used in circumstances in which the with statement does not fit the needs
-    # of the code.
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
         response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
@@ -15,7 +12,6 @@ def run():
         response = stub.SayHelloAgain(helloworld_pb2.HelloRequest(name='you'))
         print("Greeter client received: " + response.message)
         # response = stub.SayHelloForever(helloworld_pb2.HelloRequest(name='you'))
-
 
 if __name__ == '__main__':
     logging.basicConfig()

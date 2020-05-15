b950d068591597f6d2bd3004e78a2c697852c991
Ricky <etoricky@gmail.com>
2020-05-15 15:02:50 +0800

    await

--- ProtocolBuffer/nodejs/main.js
+++ ProtocolBuffer/nodejs/main.js
@@ -1,17 +1,14 @@
 const protobuf = require("protobufjs");
 
 const main = async function() {
-    protobuf.load("addressbook.proto", function(err, root) {
-        if (err)
-            throw err;
-        var Person = root.lookupType("tutorial.Person");
-        const message = Person.create({name:'Peters', id:12345});
-        console.log(message);
-        const encoded = Person.encode(message).finish();
-        console.log(encoded);
-        const decoded = Person.decode(encoded);
-        console.log(decoded);
-    });
+    const root = await protobuf.load("addressbook.proto");
+    var Person = root.lookupType("tutorial.Person");
+    const message = Person.create({name:'Peters', id:12345});
+    console.log(message);
+    const encoded = Person.encode(message).finish();
+    console.log(encoded);
+    const decoded = Person.decode(encoded);
+    console.log(decoded);
 }
 
 if (require.main===module) {
